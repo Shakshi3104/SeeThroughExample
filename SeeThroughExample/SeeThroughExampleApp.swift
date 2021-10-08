c84bf4b56b27f519d9d15c530a827565c8809a50
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2021-10-08 10:39:54 +0900

    Initial Commit

---
+++ SeeThroughExample/SeeThroughExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeThroughExampleApp.swift
+//  SeeThroughExample
+//
+//  Created by MacBook Pro M1 on 2021/10/08.
+//
+
+import SwiftUI
+
+@main
+struct SeeThroughExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
